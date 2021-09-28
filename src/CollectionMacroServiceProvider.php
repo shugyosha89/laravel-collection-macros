49a6a0dcdc28cd34f10244d26acc9657b60f3824
Matthew Bowen <matthew.bowen1989@protonmail.com>
2021-09-28 16:11:25 +0900

    Revert insertAt on master branch

--- src/CollectionMacroServiceProvider.php
+++ src/CollectionMacroServiceProvider.php
@@ -37,7 +37,6 @@ class CollectionMacroServiceProvider extends ServiceProvider
             'head' => \Spatie\CollectionMacros\Macros\Head::class,
             'ifAny' => \Spatie\CollectionMacros\Macros\IfAny::class,
             'ifEmpty' => \Spatie\CollectionMacros\Macros\IfEmpty::class,
-            'insertAt' => \Spatie\CollectionMacros\Macros\InsertAt::class,
             'ninth' => \Spatie\CollectionMacros\Macros\Ninth::class,
             'none' => \Spatie\CollectionMacros\Macros\None::class,
             'paginate' => \Spatie\CollectionMacros\Macros\Paginate::class,
